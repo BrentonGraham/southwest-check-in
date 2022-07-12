f3e53ff377151ba458fd112ddb08f048411e6cd0
Brenton Graham <bmgraham54@gmail.com>
2022-07-11 19:26:45 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name='southwest-check-in',
     version='0.0.1',
     author='Brenton Graham',
@@ -16,6 +16,17 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/BrentonGraham/southwest-check-in/issues"
     },
     license='MIT',
+    python_requires='>=3.7, <4',
     packages=['southwest_check_in'],
-    install_requires=['requests'],
+    install_requires=[
+        'click==8.1.3',
+        'DateTime==4.5',
+        'pytz==2022.1',
+        'pytz-deprecation-shim==0.1.0.post0',
+        'requests==2.28.1',
+        'selenium==4.3.0',
+        'tzdata==2022.1',
+        'tzlocal==4.2',
+        'webdriver-manager==3.8.0',
+    ]
 )
